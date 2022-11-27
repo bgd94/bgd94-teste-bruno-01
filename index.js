55635edb2da23d614891cf1fa16543ddfd987947
bgd94 <119139460+bgd94@users.noreply.github.com>
2022-11-27 02:23:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send(path.join(__dirname,'public-html/index.html')
+  res.sendFile('public-html/index.html', {root: __dirname })
 })
 
 app.listen(port, () => {
